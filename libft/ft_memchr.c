01ee10132e1990e112771b89c24b0a720a340f40
SangNyungLee~ <lsy6747@gmail.com>
2024-10-04 21:05:39 +0900

    part1

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 
-void *memchr(const void *s, int c, size_t n)
-{
+void *ft_memchr(const void *s, int c, size_t n) {
+  while (n && (*(unsigned char *) s != (unsigned char) c)) {
+    s = (unsigned char *) s + 1;
+    n--;
+  }
 
+  return (n ? (void *) s : NULL);
 }
