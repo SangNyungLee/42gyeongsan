de54b26a6f3fa32d0720fd8d3dfab1aecbe78e52
SangNyungLee~ <lsy6747@gmail.com>
2024-10-08 13:26:00 +0900

    strdup 수정

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,10 +6,11 @@
 /*   By: sanglee <sanglee@student.42gyeongsan.kr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:22:29 by sanglee           #+#    #+#             */
-/*   Updated: 2024/10/08 12:19:11 by sanglee          ###   ########.fr       */
+/*   Updated: 2024/10/08 12:51:24 by sanglee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -17,6 +18,21 @@
 char	**ft_split(char const *s, char c)
 {
 	char	*str;
+	char	**result;
+	int		len;
+	int		i;
+	int		check;
 
 	str = ft_strdup(s);
+	len = ft_strlen((char *)s);
+	i = 0;
+	while (str[i] != '\0' && (i < len))
+	{
+		if (str[i] == c)
+			str[i] = '\0';
+		i++;
+	}
+	result = (int **)malloc(sizeof(int *) * i);
+	i = 0;
+
 }
--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: sanglee <sanglee@student.42gyeongsan.kr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 16:57:15 by sanglee           #+#    #+#             */
-/*   Updated: 2024/10/08 11:16:01 by sanglee          ###   ########.fr       */
+/*   Updated: 2024/10/08 13:25:49 by sanglee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_strdup(const char *s)
 	char	*t;
 	int		len;
 
-	if (!s)
-		return (NULL);
 	len = ft_strlen((char *)s);
+	if (!len)
+		return (NULL);
 	t = (char *)malloc(len + 1);
 	ft_memcpy(t, s, len + 1);
 	return (t);
