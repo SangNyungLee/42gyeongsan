8738d5202fa4008145eb1217331db66a0aa59f06
Sangyoung Lee <sanglee@c2r17s4.42gyeongsan.kr>
2024-11-03 21:43:35 +0900

    bonus part

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanglee <sanglee@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 21:36:41 by sanglee           #+#    #+#             */
+/*   Updated: 2024/11/03 21:41:24 by sanglee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list  *ft_lstnew(void *content)
+{
+	t_list *new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+
+	return (new_node);
+}
