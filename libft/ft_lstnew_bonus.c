45b4770f8e279883ea4db567da3d480262495b79
sanglee <lsy6747@gmail.com>
2024-11-04 16:44:06 +0900

    bonus part

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanglee <sanglee@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/03 21:43:08 by sanglee           #+#    #+#             */
-/*   Updated: 2024/11/03 21:43:08 by sanglee          ###   ########.fr       */
+/*   Created: 2024/11/03 21:36:41 by sanglee           #+#    #+#             */
+/*   Updated: 2024/11/04 15:47:00 by sanglee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
